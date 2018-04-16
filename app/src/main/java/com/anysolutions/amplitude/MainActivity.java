ddc2864c133d73ef540c5e14b6143df6dd51036f
vborbely <borbelyv@gmail.com>
2018-04-16 22:21:44 +0200

    Init

---
+++ app/src/main/java/com/anysolutions/amplitude/MainActivity.java
@@ -0,0 +1,54 @@
+package com.anysolutions.amplitude;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.amplitude.api.Amplitude;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Amplitude.getInstance().initialize(this, "9d9ff2d49d2d7ec20eb41f815fb27b56").enableForegroundTracking(getApplication());
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Amplitude.getInstance().logEvent("APP_STARTED");
+    }
+
+    public void clicked(View view) {
+        Amplitude.getInstance().logEvent("BTN_CLICKED");
+    }
+}
